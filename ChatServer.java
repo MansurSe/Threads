2b5713364f36b39ac1dddcd885b090513168a610
MansurSe <84962796+MansurSe@users.noreply.github.com>
2021-08-21 02:02:46 +0600

    Add files via upload
    commit

---
+++ ChatServer.java
@@ -0,0 +1,48 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ChatServer {
+    ArrayList<Client> clients = new ArrayList<>();
+    ServerSocket serverSocket;
+
+    ChatServer() throws IOException{
+
+        // создаем серверный сокет на порту 1234
+        serverSocket = new ServerSocket(1234);
+    }
+
+    // рассылка текста по всем клиентам
+    void sendAll(String message){
+        for(Client client: clients){
+            client.receive(message);
+        }
+    }
+
+
+    public void run(){
+        while(true) {
+            System.out.println("Waiting connection...");
+
+        try{
+            // ждем клиента из сети
+            Socket socket = serverSocket.accept();
+            System.out.println("Client connected!");
+
+            // создаем клиента на своей стороне и добавляем его в ArrayList
+            clients.add(new Client(socket,this));
+
+
+        } catch (IOException e){
+            e.printStackTrace();
+            }
+        }
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        new ChatServer().run();
+    }
+
+}
